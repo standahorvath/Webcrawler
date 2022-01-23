9ef2c7a48df7db4ff4b46de4315f0febd000c5dc
standahorvath <standa3@email.cz>
2022-01-23 20:20:23 +0100

    coment sitemap

--- index.js
+++ index.js
@@ -4,6 +4,17 @@ var crawler = require('./modules/crawler.js');
 
 
 
+/* Crawling sitemap */
+/*
+let startingUrl = "https://www.blizzard.com"
+
+crawler.run(startingUrl, "sitemap", (sitemap) => {
+    fs.writeFile('./sitemap.xml', sitemap, 'utf8', function (err) {
+        if (err) return console.log(err);
+    })
+})
+*/
+
 let startingUrl = "https://www.blizzard.com"
 
 crawler.run(startingUrl, "sitemap", (sitemap) => {
